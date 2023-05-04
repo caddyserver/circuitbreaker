c38ace9b9f322b51eebecc2d59987d0cef8c2065
Mohammed Al Sahaf <msaa1990@gmail.com>
2023-05-05 01:26:02 +0300

    chore: update dependencies
    Fixes #1

--- circuitbreaker.go
+++ circuitbreaker.go
@@ -23,7 +23,7 @@ import (
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp/reverseproxy"
-	"github.com/diamondburned/oxy/memmetrics"
+	"github.com/vulcand/oxy/memmetrics"
 )
 
 func init() {
